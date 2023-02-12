3089399bedbfcc1a09a996c50052b08674ffe486
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-02-12 21:36:50 +0800

    longer timeout

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 Xvfb $DISPLAY -screen $DISPLAY 640x480x8 &
-gunicorn -b 0.0.0.0:80 main:server --timeout 200 --worker-class gevent
+gunicorn -b 0.0.0.0:80 main:server --timeout 300 --worker-class gevent

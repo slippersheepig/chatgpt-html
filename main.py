4f812af1b87221b961d42c68e75e1f11ba25dfa9
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-02-19 23:35:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,27 +10,20 @@ with open("config.json", "r") as f: config = json.load(f)
 # init chatbot
 chatbot = Chatbot(config)
 
-def send_gpt(message):
+def generate_response(prompt):
     prev_text = ""
-    for data in chatbot.ask(message):
+    for data in chatbot.ask(prompt):
         prev_text = prev_text + data["message"][len(prev_text) :]
     return prev_text
 
-@server.route('/', methods=['GET', 'POST'])
-def get_request_json():
-    if request.method == 'POST':
-        if len(request.form['question']) < 1:
-            return render_template(
-                'chat.html', question="null", res="问题不能为空")
-        question = request.form['question']
-        print("======================================")
-        print("接到请求:", question)
-        res = send_gpt(question)
-        print("问题：\n", question)
-        print("答案：\n", res)
+@server.route("/")
+def home():
+    return render_template("chat.html")
 
-        return render_template('chat.html', question=question, res=str(res))
-    return render_template('chat.html', question=0)
+@server.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    return str(generate_response(user_text))
 
 if __name__ == '__main__':
     server.run(debug=False, host='0.0.0.0', port=8088)
